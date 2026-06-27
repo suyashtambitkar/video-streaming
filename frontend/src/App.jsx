be9ab2b31b8395030d549ca8b734b7f1fa277e15
suyashtambitkar <suyashtambitkar9@gmail.com>
2026-06-27 15:40:37 +0530

    Remove unused React hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Route/Login";
 import Registration from "./Route/Registration";
